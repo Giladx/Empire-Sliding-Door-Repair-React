63255de17a50887151ee82ae5c5959a1ca2acc95
Gilad Levi <gilad.x@gmail.com>
2024-07-01 16:49:04 +0300

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,285 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>
+          Empire sliding door repair Florida - Glide through life seamlessly
+        </title>
+        <meta
+          name="description"
+          content="Welcome to Empire Sliding Door Repair Florida, 24/7! Locations: Homestead: +1 (305) 407-9566, Miami: +1 (786) 348-2109, Boynton Beach: +1 (786) 348-2109."
+        />
+        <meta
+          property="og:title"
+          content="Empire sliding door repair Florida - Glide through life seamlessly"
+        />
+        <meta
+          property="og:description"
+          content="Welcome to Empire Sliding Door Repair Florida, 24/7! Locations: Homestead: +1 (305) 407-9566, Miami: +1 (786) 348-2109, Boynton Beach: +1 (786) 348-2109."
+        />
+        <meta
+          property="og:image"
+          content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/e3fe0375-fc20-4faf-af75-f6f72d0e002c/b4eab0d1-8134-4679-a4c4-f63407058500?org_if_sml=1&amp;q=80&amp;force_format=original"
+        />
+      </Helmet>
+      <header className="home-container1">
+        <header data-thq="thq-navbar" className="home-navbar-interactive">
+          <Link to="/" className="home-navlink">
+            <img
+              alt="Sliding Door Repair Professionals"
+              sizes="(min-width: 992px) 1200px, (min-width: 768px) 800px, 480px"
+              src="/empire-1500w.webp"
+              srcSet="/empire-1500w.webp 1200w, /empire-tablet.webp 800w, /empire-mobile.webp 480w"
+              className="home-image1"
+            />
+          </Link>
+          <div data-thq="thq-navbar-nav" className="home-desktop-menu"></div>
+          <div data-thq="thq-burger-menu" className="home-burger-menu"></div>
+          <div data-thq="thq-mobile-menu" className="home-mobile-menu">
+            <div className="home-nav">
+              <div className="home-top">
+                <img
+                  alt="Sliding Door Repair Professionals"
+                  sizes="(min-width: 992px) 1200px, (min-width: 768px) 800px, 480px"
+                  src="/empire-1500w.webp"
+                  srcSet="/empire-1500w.webp 1200w, /empire-tablet.webp 800w, /empire-mobile.webp 480w"
+                  className="home-logo"
+                />
+                <div data-thq="thq-close-menu" className="home-close-menu">
+                  <svg viewBox="0 0 1024 1024" className="home-icon">
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="home-links">
+                <span>Home</span>
+                <span>Services</span>
+                <span className="thq-body-small thq-link">About Us</span>
+                <span className="thq-body-small thq-link">Contact</span>
+                <span className="thq-body-small thq-link">Link5</span>
+              </nav>
+            </div>
+            <div className="home-buttons">
+              <button className="thq-button-filled">Login</button>
+              <button className="thq-button-outline">Register</button>
+            </div>
+          </div>
+        </header>
+      </header>
+      <div className="home-header30 thq-section-padding">
+        <img
+          alt="Sliding Door Repair Professionals"
+          sizes="(min-width: 992px) 1200px, (min-width: 768px) 800px, 480px"
+          src="/photo-1497366754035-f200968a6e723-1500w.webp"
+          srcSet="/photo-1497366754035-f200968a6e723-1500w.webp 1200w, /photo-1497366754035-f200968a6e723-tablet.webp 800w, /photo-1497366754035-f200968a6e723-mobile.webp 480w"
+          className="home-image"
+        />
+        <div className="home-container2"></div>
+        <div className="home-max-width thq-section-max-width">
+          <div className="home-content">
+            <h1 className="home-text thq-heading-1">
+              <span className="home-text01">
+                Empire Sliding Door Repair Florida
+              </span>
+              <br></br>
+              <br></br>
+            </h1>
+            <p className="home-text04 thq-body-large">
+              Glide Through Life Seamlessly
+            </p>
+            <div className="home-actions"></div>
+          </div>
+        </div>
+      </div>
+      <div className="home-layout251 thq-section-padding">
+        <div className="home-max-width1 thq-section-max-width">
+          <div className="home-section-title thq-flex-row">
+            <div className="home-column thq-flex-column">
+              <span className="home-text05 thq-body-small">
+                Quality Repairs, Exceptional Service
+              </span>
+              <h2 className="home-text06 thq-heading-2">
+                Sliding door Services
+              </h2>
+            </div>
+            <span className="home-text07 thq-body-small">
+              Discover the key services that make our sliding door repair
+              services stand out.
+            </span>
+          </div>
+          <div className="home-content01">
+            <div className="home-row thq-flex-row">
+              <div className="home-feature1 thq-flex-column">
+                <img
+                  alt="fix sliding glass door"
+                  sizes="(min-width: 992px) 1200px, (min-width: 768px) 800px, 480px"
+                  src="/photo-1610333684078-c89bd57f2e462-1400w.webp"
+                  srcSet="/photo-1610333684078-c89bd57f2e462-1400w.webp 1200w, /photo-1610333684078-c89bd57f2e462-tablet.webp 800w, /photo-1610333684078-c89bd57f2e462-mobile.webp 480w"
+                  className="thq-img-ratio-4-3 home-feature1-image"
+                />
+                <div className="home-content02 thq-flex-column">
+                  <h2 className="home-feature1-title thq-heading-3">
+                    Fix sliding glass door
+                  </h2>
+                  <span className="thq-body-small">
+                    Our team consists of highly skilled technicians with years
+                    of experience in repairing sliding doors.
+                  </span>
+                </div>
+              </div>
+              <div className="home-feature2 thq-flex-column">
+                <img
+                  alt="replace sliding door"
+                  src="/photo-1574396632235-343dac3993b1-300h.webp"
+                  className="thq-img-ratio-4-3 home-feature2-image"
+                />
+                <div className="home-content03 thq-flex-column">
+                  <h2 className="home-feature2-title thq-heading-3">
+                    Glassdoor repair
+                  </h2>
+                  <span className="thq-body-small">
+                    We understand the urgency of sliding door repairs,
+                    that&apos;s why we offer quick turnaround times for our
+                    services.
+                  </span>
+                </div>
+              </div>
+              <div className="home-feature3 thq-flex-column">
+                <img
+                  alt="replace sliding door"
+                  src="/photo-1534430198509-8a3091682aa8-300h.webp"
+                  className="thq-img-ratio-4-3 home-feature3-image"
+                />
+                <div className="home-content04 thq-flex-column">
+                  <h2 className="home-feature3-title thq-heading-3">
+                    Replace sliding door
+                  </h2>
+                  <span className="thq-body-small">
+                    Customer satisfaction is our top priority. We strive to
+                    exceed expectations and ensure a seamless repair experience.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-actions1"></div>
+        </div>
+      </div>
+      <div className="home-contact20 thq-section-padding">
+        <div className="home-max-width2 thq-section-max-width">
+          <div className="home-section-title1">
+            <span className="thq-body-small">Get in touch with us today!</span>
+            <div className="home-content05">
+              <h2 className="home-text09 thq-heading-2">Contact Us</h2>
+            </div>
+          </div>
+          <div className="home-row1">
+            <a href="tel:+13054079566" className="home-link">
+              <div className="home-content06">
+                <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                  <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                </svg>
+                <div className="home-contact-info">
+                  <div className="home-content07">
+                    <h3 className="home-text10 thq-heading-3">
+                      Empire sliding door repair - Homestead ,Florida
+                    </h3>
+                    <p className="home-text11 thq-body-large">
+                      <span>14037 SW 274th Terr</span>
+                      <br></br>
+                      <span>Homestead, FL 33032</span>
+                      <br></br>
+                      <span>United States</span>
+                    </p>
+                  </div>
+                  <span className="home-address thq-body-small">
+                    +13054079566
+                  </span>
+                </div>
+              </div>
+            </a>
+            <a
+              href="tel:+17863482109"
+              className="thq-body-small thq-link home-link1"
+            >
+              <div className="home-content08">
+                <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                  <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                </svg>
+                <div className="home-contact-info1">
+                  <div className="home-content09">
+                    <h3 className="home-text17 thq-heading-3">
+                      <span>Empire sliding door repair -</span>
+                      <br></br>
+                      <span> Maiami ,Florida</span>
+                    </h3>
+                    <p className="home-text21 thq-body-large">
+                      <span>2980 NW 169th Terr</span>
+                      <br></br>
+                      <span>Miami Gardens, FL 33056</span>
+                      <br></br>
+                      <span>United States</span>
+                    </p>
+                  </div>
+                  <span className="home-address1 thq-body-small">
+                    +17863482109
+                  </span>
+                </div>
+              </div>
+            </a>
+            <a
+              href="tel:+19542374136"
+              className="thq-body-small thq-link home-link2"
+            >
+              <div className="home-content10">
+                <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                  <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                </svg>
+                <div className="home-contact-info2">
+                  <div className="home-content11">
+                    <h3 className="home-text27 thq-heading-3">
+                      Empire sliding door repair - Boynton Beach ,Florida
+                    </h3>
+                    <p className="home-text28 thq-body-large">
+                      <span>
+                        11366 Barca Blvd,
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>
+                        Boynton Beach, FL 33437,
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <br></br>
+                      <span>United States</span>
+                    </p>
+                  </div>
+                  <span className="home-address2 thq-body-small">
+                    +19542374136
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
